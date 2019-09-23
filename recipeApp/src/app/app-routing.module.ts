45fc04ae2d4c42aa0aabb0dcf8af2c2e16b619d8
Gurjot Singh <gurjot@myerrunds.com>
2019-09-23 14:46:15 -0400

    created a header and added a routing links

--- recipeApp/src/app/app-routing.module.ts
+++ recipeApp/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RecipesComponent } from "./components/recipes/recipes.component";
+import { CreateComponent } from "./components/create/create.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: RecipesComponent },
+  { path: "create", component: CreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- recipeApp/src/app/app.module.ts
+++ recipeApp/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './Components/header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./Components/header/header.component";
+import { RecipesComponent } from "./components/recipes/recipes.component";
+import { CreateComponent } from "./components/create/create.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    CreateComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
